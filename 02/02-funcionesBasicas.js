401b94fcdb9c3e607c2b466b4b7d491352be9966
Isadora Matias <izzadora@outlook.com>
2024-01-26 15:50:39 +0100

    isadora

---
+++ 02/02-funcionesBasicas.js
@@ -0,0 +1,15 @@
+//FUNCIONES
+
+//sumar 5
+function sumarCinco(numero) {
+    const resultado = numero + 5
+    console.log(`Resultado de sumar 5: ${resultado}`)
+    return resultado
+}
+
+//MULTIPLICAR 2
+
+//RESTAR 3
+
+//LLAMAR FUNCIONES
+const resultadoSuma = sumarCinco(2)
