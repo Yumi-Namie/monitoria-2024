edab3ade1d6d2537360c854658ce6e529de7559c
Yumi-Namie <rn.yumi@gmail.com>
2024-03-01 16:30:26 +0100

    folder 06

--- 06/while.js
+++ 06/while.js
@@ -1,29 +1,44 @@
 /*
 01 -Te encuentras en una tienda de frutas y deseas imprimir en mayúsculas el nombre de cada fruta en tu lista hasta que encuentres la fruta "uva". Una vez que encuentres la "uva", quieres que el programa se detenga y muestre un mensaje indicando que la encontraste.
 */
-let frutas = ["manzana", "banana", "uva", "fresa", "pera"];
-let indice = 0
-function frutas () {
-    while (indice < frutas.length){
-        //transformar palabras en uppercase
-        let frutasMayuscula = frutas[indice].toUpperCase();
-        console.log("fruta " + frutasMayuscula);
-        indice ++;
-        
-        // imprimir mensaje cuando encontrar uva
-        if (frutas[indice] === "uva") {
-            console.log("Encontramos la uva")
-            break
-        }
-    }
-}
+// let frutas = ["manzana", "banana", "uva", "fresa", "pera"];
+// let indice = 0
+// function frutas () {
+//     while (indice < frutas.length){
+//         //transformar palabras en uppercase
+//         let frutasMayuscula = frutas[indice].toUpperCase();
+//         console.log("fruta " + frutasMayuscula);
+//         indice ++;
 
-
+//         // imprimir mensaje cuando encontrar uva
+//         if (frutas[indice] === "uva") {
+//             console.log("Encontramos la uva")
+//             break
+//         }
+//     }
+// }
 
 
 /*
 Imagina que estás contando billetes en una caja registradora y quieres imprimir cada cantidad hasta que alcances un total de 180 dólares. → billetes [100, 50, 20, 10, 5]
+
  */
+
+let cartera = [100, 50, 20, 10, 5];
+let total = 0;
+let indicebillete = 0;
+
+while (indicebillete < cartera.length) {
+    total += cartera[indicebillete];
+    console.log("el total es" + total);
+
+    if (total == 180) {
+        break
+    }
+    indicebillete++;
+}
+
+
 
 /*
 Supongamos que estás trabajando con una lista de números y deseas imprimir cada número hasta que encuentres el número 7. Cuando encuentres el 7, deseas que el programa se detenga y muestre un mensaje indicando que lo encontraste.
@@ -32,3 +47,9 @@ Supongamos que estás trabajando con una lista de números y deseas imprimir cad
 /*
 Imagina que estás organizando una carrera de animales y quieres simular el proceso de contar cuántos animales han cruzado la línea de meta. Quieres imprimir el nombre de cada animal y su posición en la carrera hasta que todos los animales hayan terminado.
 */
+let animales = ["tortuga", "pajaro", "gato", "perro", "liebre"]
+let posicion = ;
+
+while ( posicion <= animales.length) {
+
+}
